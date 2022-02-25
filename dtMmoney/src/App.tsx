838b732e89cebc97878bb1a4997fe5551214f1c4
Rubens Henrique Pereira <rubensh206@gmail.com>
2022-02-24 21:20:21 -0300

    dtMoney

---
+++ dtMmoney/src/App.tsx
@@ -0,0 +1,18 @@
+import { GlobalStyle } from "./styles/global"
+import { Header } from '../components/Header/index';
+import { Dashboard } from '../components/Dashboard/index';
+
+
+
+export function App() {
+
+  return (
+    <>
+      <Header />
+      <Dashboard />
+      <GlobalStyle />
+    </>
+        
+  )
+}
+
